bb3a61ba0b38d918834c8963b28954fcd3f8dcb7
30185529 <Jose.figueredo@ucalgary.ca>
2026-04-16 03:18:00 -0600

    updated views

--- pc_builder/pc_builder_app/views.py
+++ pc_builder/pc_builder_app/views.py
@@ -1,25 +1,47 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from .models import Build, BuildItem, Component
 
-# Create your views here.
+
 def home(request):
     return render(request, "index.html")
 
 
 def login(request):
-    return render(request, 'customer_login.html')
+    return render(request, "customer_login.html")
+
 
 def register(request):
-    return render(request, 'customer_registeration.html')
+    return render(request, "customer_registeration.html")
+
 
 def builder(request):
-    return render(request, 'builder.html')
+    build, created = Build.objects.get_or_create(id=1, defaults={"name": "My Build"})
+    items = BuildItem.objects.filter(build=build)
+
+    return render(request, "builder.html", {"items": items})
+
+
+def add_component(request, component_id):
+    build = Build.objects.get(id=1)
+    component = Component.objects.get(id=component_id)
+
+    BuildItem.objects.create(build=build, component=component)
+    return redirect("builder")
+
+
+def remove_component(request, item_id):
+    BuildItem.objects.filter(id=item_id).delete()
+    return redirect("builder")
+
 
 def search(request):
-    return render(request, 'search_page.html')
+    components = Component.objects.all()
+    return render(request, "search_page.html", {"components": components})
+
 
 def supplier_inventory(request):
-    return render(request, 'supplier_inventory_page.html')
+    return render(request, "supplier_inventory_page.html")
+
 
 def manufacturer(request):
-    return render(request, 'manufacturer_selection_page.html')
-
+    return render(request, "manufacturer_selection_page.html")
