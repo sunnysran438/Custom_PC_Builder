27d8a8e89361b7af7cd5ee07ccade75d628d5778
30185529 <Jose.figueredo@ucalgary.ca>
2026-04-16 03:17:29 -0600

    updated admin

--- pc_builder/pc_builder_app/admin.py
+++ pc_builder/pc_builder_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Manufacturer, Supplier, Component, StoreItem, Build, BuildItem
 
-# Register your models here.
+
+admin.site.register(Manufacturer)
+admin.site.register(Supplier)
+admin.site.register(Component)
+admin.site.register(StoreItem)
+admin.site.register(Build)
+admin.site.register(BuildItem)
